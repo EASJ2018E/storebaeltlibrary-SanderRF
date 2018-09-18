0086b59bb7960378f6615b59653224b7e602c34e
Sander <33893015+SanderRF@users.noreply.github.com>
2018-09-18 18:22:37 +0200

    Manual Upload

---
+++ StoreBaeltTicketLibrary/StoreBaeltTicketTest/StoreBealtTicketTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StoreBaeltTicketTest
+{
+    [TestClass]
+    public class StoreBealtTicketTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
